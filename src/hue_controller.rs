4d34bda61666ed8eba689831e1ece4117eac9624
mazhewitt <mazda@aduki.co.uk>
2023-03-30 11:28:09 +0200

    Added BridgeInfo

--- src/hue_controller.rs
+++ src/hue_controller.rs
@@ -4,6 +4,17 @@ use std::io;
 use std::io::{Error, Write};
 use mac_address::get_mac_address;
 use hueclient;
+use hueclient::HueError;
+
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BridgeInfo {
+    ip_addr: String,
+    client_token: String,
+}
+
 
 // A function that takes a file path and returns a Result<String>
 fn read_hue_token_from_file(path: &str) -> io::Result<String> {
@@ -19,10 +30,38 @@ fn write_hue_token_to_file(path: &str, content: &str) -> Result<(), Error> {
     write!(file, "{}", content)
 }
 
+// write me a function that initializes the bridge using the devices mac address and stores the client token in a file, Return a Result<Bridge>
+
+pub fn initialize_bridge(bridge_info: &BridgeInfo) -> Result<hueclient::Bridge, HueError> {
+    let bridge = hueclient::Bridge::new(bridge_info.ip_addr.as_str(), bridge_info.client_token.as_str());
+    Ok(bridge)
+}
+
+fn register_hue_client() -> Result<BridgeInfo, HueError> {
+    let mac_addr = get_mac_address().expect("Could not get mac address");
+    let mac_addr_str = mac_addr.unwrap().to_string();
+    let bridge = hueclient::Bridge::discover_required()
+        .register_user(&mac_addr_str)?; // Press the bridge before running this
+    let token = bridge.username.clone();
+    let ip_addr = bridge.ip.to_string();
+    let bridge_info = BridgeInfo { ip_addr, client_token: token };
+    println!("the username was {}, IP was {}", bridge_info.client_token, bridge_info.ip_addr );
+    Ok(bridge_info)
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // a unit test to test register_hue_client // have to press the hue button before running this test
+    #[test]
+    fn can_register_hue_client() {
+        let bridge_info = register_hue_client().unwrap();
+        assert!(bridge_info.ip_addr.len() > 0);
+        assert!(bridge_info.client_token.len() > 0);
+    }
+
 
     #[test]
     fn write_and_read_hue_token_to_from_file() {
@@ -52,6 +91,7 @@ mod tests {
         let maybe_mac_string = get_mac_address();
         let found_mac_addr = match maybe_mac_string {
             Ok(Some(_ma)) => {
+                println!("{}", _ma.to_string());
                 true
             }
             Ok(None) => {
