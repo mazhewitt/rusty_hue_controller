34df5fc5b798a39ce494161f58f66aa97a6128dd
mazhewitt <mazda@aduki.co.uk>
2023-03-30 14:24:29 +0200

    Can turn the lights on and off

--- src/hue_controller.rs
+++ src/hue_controller.rs
@@ -4,28 +4,33 @@ use std::io;
 use std::io::{Error, Write};
 use mac_address::get_mac_address;
 use hueclient;
-use hueclient::HueError;
+use hueclient::{CommandLight, HueError};
 
 use serde::{Serialize, Deserialize};
 
 
 #[derive(Serialize, Deserialize, Debug)]
-struct BridgeInfo {
+pub struct BridgeInfo {
     ip_addr: String,
     client_token: String,
 }
 
 
 // A function that takes a file path and returns a Result<String>
-fn read_hue_token_from_file(path: &str) -> io::Result<String> {
+pub fn read_bridge_info_from_file(path: &str) -> io::Result<BridgeInfo> {
     // Use fs::read_to_string to read the file contents into a String
-    fs::read_to_string(path)
+    let content = fs::read_to_string(path)?;
+    //deserialize the bridge info
+    let bridge_info: BridgeInfo = serde_json::from_str(&content).unwrap();
+    Ok(bridge_info)
 }
 
 // A function that takes a file path and a string and returns a Result<()>
-fn write_hue_token_to_file(path: &str, content: &str) -> Result<(), Error> {
+pub fn write_bridge_info_to_file(path: &str, bridge_info: BridgeInfo) -> Result<(), Error> {
     // Use File::create to create or truncate a file
     let mut file = File::create(path)?;
+    //serialize the bridge info
+    let content = serde_json::to_string(&bridge_info).unwrap();
     // Use write! macro to write the content into the file
     write!(file, "{}", content)
 }
@@ -33,11 +38,11 @@ fn write_hue_token_to_file(path: &str, content: &str) -> Result<(), Error> {
 // write me a function that initializes the bridge using the devices mac address and stores the client token in a file, Return a Result<Bridge>
 
 pub fn initialize_bridge(bridge_info: &BridgeInfo) -> Result<hueclient::Bridge, HueError> {
-    let bridge = hueclient::Bridge::new(bridge_info.ip_addr.as_str(), bridge_info.client_token.as_str());
-    Ok(bridge)
+    let bridge = hueclient::Bridge::for_ip(std::net::IpAddr::V4(bridge_info.ip_addr.parse().unwrap()));
+    Ok(bridge.with_user(bridge_info.client_token.clone()))
 }
 
-fn register_hue_client() -> Result<BridgeInfo, HueError> {
+pub fn register_hue_client() -> Result<BridgeInfo, HueError> {
     let mac_addr = get_mac_address().expect("Could not get mac address");
     let mac_addr_str = mac_addr.unwrap().to_string();
     let bridge = hueclient::Bridge::discover_required()
@@ -49,10 +54,37 @@ fn register_hue_client() -> Result<BridgeInfo, HueError> {
     Ok(bridge_info)
 }
 
+// a function that takes a bridge and the name of a group and toggeles the light state of that group
+pub fn toggle_group(bridge: hueclient::Bridge, group_name: &str) -> Result<(), HueError> {
+    let groups = bridge.get_all_groups()?;
+    for i_group in groups {
+        let group = i_group.group;
+        if group.name == group_name {
+            let light_state = group.state.clone();
+            let command = match light_state.all_on {
+                true => CommandLight::off(Default::default()),
+                false => CommandLight::on(Default::default()),
+            };
+            bridge.set_group_state(i_group.id, &command)?;
+        }
+    }
+    Ok(())
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    // a unit test to test toggle_group
+    #[test]
+    fn can_toggle_group() {
+        let bridge_info = read_bridge_info_from_file("bridge_info.json").unwrap();
+        let bridge = initialize_bridge(&bridge_info).unwrap();
+        let result = toggle_group(bridge, "Study");
+        assert!(result.is_ok());
+    }
+
 
     // a unit test to test register_hue_client // have to press the hue button before running this test
     #[test]
@@ -60,20 +92,7 @@ mod tests {
         let bridge_info = register_hue_client().unwrap();
         assert!(bridge_info.ip_addr.len() > 0);
         assert!(bridge_info.client_token.len() > 0);
-    }
-
-
-    #[test]
-    fn write_and_read_hue_token_to_from_file() {
-        // Use a sample file and string for testing
-        let path = "token_file.txt";
-        let content = "HDJHJDHJKHDJWKSHJKDJKFDHEW";
-        // Call the function and expect it to return Ok(())
-        write_hue_token_to_file(path, content).expect("Should have been able to write to the file");
-        // Call the read_string_from_file function and expect it to return Ok with the same content
-        let result = read_hue_token_from_file(path).expect("Should have been able to read from the file");
-        // Assert that the result equals the content
-        assert_eq!(result, content);
+        write_bridge_info_to_file("bridge_info.json", bridge_info).unwrap();
     }
 
     #[test]
